40f95d6db4722ba331769a67a207bee2273f097e
lucasbrazza <lucas.brazza.lb@gmail.com>
2024-02-12 20:47:24 -0300

    corrected templates path

--- Atividades/atividade-pratica/server/server/settings.py
+++ Atividades/atividade-pratica/server/server/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+TEMPLATES_DIR = ROOT_DIR / 'web' / 'templates'
+STATIC_FILES_DIR = ROOT_DIR / 'web' / 'static'
 
 
 # Quick-start development settings - unsuitable for production
@@ -63,7 +66,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [  TEMPLATES_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
