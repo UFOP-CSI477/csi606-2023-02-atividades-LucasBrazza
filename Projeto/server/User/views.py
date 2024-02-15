a0c88cb8016018a62f5411c4be21eaba769eea8f
lucasbrazza <lucas.brazza.lb@gmail.com>
2024-02-15 11:31:04 -0300

    created login logout views

--- Projeto/server/User/views.py
+++ Projeto/server/User/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import authenticate, login,  logout
+from django.shortcuts import redirect
+from django.views.generic import View
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from .models import UserDriverModel, UserClientModel
 from .serializers import UserDriverSerializer, UserClientSerializer
 from . import permissions
+
 
 class CreateDriverUserView(generics.CreateAPIView):
     queryset = UserDriverModel.objects.all()
@@ -59,3 +63,20 @@ class UpdateClientUserView(generics.UpdateAPIView):
             self.perform_update(serializer)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(View):
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirecione para a página inicial após o login
+        else:
+            return redirect('create_user')  # Redirecione para a página de criação de usuário se o login falhar
+        
+        
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')  # Redirecione para a página de login após o logout
