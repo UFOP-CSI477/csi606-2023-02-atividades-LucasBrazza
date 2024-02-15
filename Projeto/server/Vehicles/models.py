853f996e27ec7f759049744aaf7f0824b8985c80
lucasbrazza <lucas.brazza.lb@gmail.com>
2024-02-14 21:58:42 -0300

    created Vehicle and started some basic file wrinting

--- Projeto/server/Vehicles/models.py
+++ Projeto/server/Vehicles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class VehicleType(Enum):
+    CAR = "CAR"
+    BUS = "BUS"
+    VAN = "Van"
+
+class Vehicle(models.Model):
+    
+    ENUM_VECHICLE_TYPE = ()
+    
+    id = models.AutoField(primary_key=True)
+    plate = models.CharField(max_length=7, unique=True, required=True)
+    type =  models.CharField(max_length=30, choices=VehicleType, required=True)
+    seats_quantity = models.IntegerField(required=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.type} - {self.plate}'
--- Projeto/server/Vehicles/views.py
+++ Projeto/server/Vehicles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from .models import Vehicle
+
+class VehicleList(ListView):
+    model = Vehicle
+    template_name = 'vehicle_list.html'
+    context_object_name = 'vehicles'
+    
+    def get_queryset(self):
+        return Vehicle.objects.all()
+    
+    
+    
