308ea9a6096adfcbb2f25b9840f475229b76f6ee
lucasbrazza <lucas.brazza.lb@gmail.com>
2024-02-17 01:01:28 -0300

    updated trip model

--- Projeto/server/Trip/models.py
+++ Projeto/server/Trip/models.py
@@ -7,15 +7,17 @@ class TripModel(models.Model):
     origen = models.CharField(max_length=100, blank=False)
     destination = models.CharField(max_length=100, blank=False)
     day = models.DateField(blank=False)
-    schedule = models.TimeField(blank=False)
+    scheduled_time = models.TimeField(blank=False)
     driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
     vehicle = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
     vacancies = models.IntegerField(default=Vehicle.seats_quantity)
     seats_taken = models.IntegerField(default=Vehicle.seats_quantity - vacancies)
-    passengers = models.ManyToManyField('Passenger', blank=True, through='PassengerTripMode')
+    passengers = models.ManyToManyField('Passenger', null=True, blank=True, through='PassengerTripMode')
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=False)
     
     
-class PassengerTripMode(models.Model):
+class PassengerTripModel(models.Model):
+    id = models.AutoField(primary_key=True)
     trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
     passenger = models.ForeignKey('Passenger', on_delete=models.CASCADE)
     
