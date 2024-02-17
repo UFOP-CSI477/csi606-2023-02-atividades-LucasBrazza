6585855d20da42697cb8df62694cac2bdaf03532
lucasbrazza <lucas.brazza.lb@gmail.com>
2024-02-16 23:07:44 -0300

    added many to many field

--- Projeto/server/Trip/models.py
+++ Projeto/server/Trip/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from Vehicles.models import Vehicle
 
 
-class Trip(models.Model):
+class TripModel(models.Model):
     id = models.AutoField(primary_key=True)
-    origen = models.CharField(max_length=100, required=True)
-    destination = models.CharField(max_length=100, required=True)
-    day = models.DateField(required=True)
-    schedule = models.TimeField(required=True)
+    origen = models.CharField(max_length=100, blank=False)
+    destination = models.CharField(max_length=100, blank=False)
+    day = models.DateField(blank=False)
+    schedule = models.TimeField(blank=False)
     driver = models.ForeignKey('Driver', on_delete=models.CASCADE)
     vehicle = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
     vacancies = models.IntegerField(default=Vehicle.seats_quantity)
-    seats_taken = models.IntegerField(default=0)
+    seats_taken = models.IntegerField(default=Vehicle.seats_quantity - vacancies)
+    passengers = models.ManyToManyField('Passenger', blank=True, through='PassengerTripMode')
+    
+    
+class PassengerTripMode(models.Model):
+    trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
+    passenger = models.ForeignKey('Passenger', on_delete=models.CASCADE)
     
